db55d5268fef0b91ee3461d4feac1e6585783019
vikram singh shekhawat <vshekhawat@habana.ai>
2025-11-05 12:14:55 +0530

    Fix Pre-commit code formating. (#18)
    Co-authored-by: svc_repro_tool <svc_repro_tool@habana.ai>

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -112,10 +112,10 @@ from sglang.srt.model_executor.cpu_graph_runner import CPUGraphRunner
 from sglang.srt.model_executor.cuda_graph_runner import CudaGraphRunner
 from sglang.srt.model_executor.forward_batch_info import ForwardBatch, PPProxyTensors
 from sglang.srt.model_executor.npu_graph_runner import NPUGraphRunner
-from sglang.srt.model_executor.xpu_graph_runner import XPUGraphRunner
 from sglang.srt.model_executor.piecewise_cuda_graph_runner import (
     PiecewiseCudaGraphRunner,
 )
+from sglang.srt.model_executor.xpu_graph_runner import XPUGraphRunner
 from sglang.srt.model_loader import get_model
 from sglang.srt.model_loader.loader import DefaultModelLoader, get_model_loader
 from sglang.srt.model_loader.remote_instance_weight_loader_utils import (
